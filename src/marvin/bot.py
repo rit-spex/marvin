2ea7d87984c1972a7a9b4754e7d3199179b0c80e
8xKetch <stevie.6strings@gmail.com>
2021-12-02 00:34:24 -0500

    added initial documentation, implemented safeguard in on_message() for bot messages.

--- src/marvin/bot.py
+++ src/marvin/bot.py
@@ -1,17 +1,40 @@
+''' A bot for the SPEX discord. 
+
+Authors: Joshua Yoder, Stevie Alvarez
+'''
 import os
 
 import discord
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
+    """ Called when bot successfully loged in and 'ready'. """
+
     print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
+    """ Called when message sent to accessable text channel.
+
+    Args:
+        message: message recieved from guild. discord.Message instance. 
+            NOTE: for more info on message objects: 
+            https://discordpy.readthedocs.io/en/stable/api.html#discord.Message
+
+    Pre:
+        Intents.message must be enabled.
+
+    TODO: handle 'guild' and dm messages separatly, can cause internal issues.
+    """
+
+    if message.author == client.user:  # don't process messages from this bot
+        return
+    
     print(message.content)
 
     if message.content == 'marvin, are you alive?':
