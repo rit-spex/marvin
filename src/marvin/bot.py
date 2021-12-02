54cec1f3ada1cdeff0b1152adef9397c432089e6
Joshua Yoder <me@jpfyoder.com>
2021-12-02 01:40:59 +0000

    add: basic read and reponse

--- src/marvin/bot.py
+++ src/marvin/bot.py
@@ -10,6 +10,13 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    print(message.content)
+
+    if message.content == 'marvin, are you alive?':
+        await message.channel.send('Life? Don\'t talk to me about life.')
+
 # run bot
 if __name__ == '__main__':
     client.run(TOKEN)
