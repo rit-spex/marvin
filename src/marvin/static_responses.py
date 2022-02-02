7687cfdb8e790b37fe72b9be1b3cb80d880f30fc
Mr.Fumpledump <37516477+Fumpledump@users.noreply.github.com>
2022-02-01 19:21:12 -0500

    some slackbot commands added

--- src/marvin/static_responses.py
+++ src/marvin/static_responses.py
@@ -1,4 +1,10 @@
 STATIC_RESPONSES = {
     "google meat": "🍖🥩https://meet.google.com/dof-drim-for 🥩🍖",
+    "SR-71": ":airplane_departure::speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :turtle:\n:helicopter::speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :steam_locomotive:\n⚓️:speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :bullettrain_side:\n⚓️:speech_balloon: :sunglasses:\n:airplane::speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :rocket:\n:airplane::speech_balloon: :point_right: :stars:\n:japanese_castle::speech_balloon: :thumbsup: :clap::clap::clap::clap:\n:airplane::speech_balloon: :clap::clap::clap::clap:\n",
+    "sr-71": ":airplane_departure::speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :turtle:\n:helicopter::speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :steam_locomotive:\n⚓️:speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :bullettrain_side:\n⚓️:speech_balloon: :sunglasses:\n:airplane::speech_balloon: :rabbit2::question:\n:japanese_castle::speech_balloon: :rocket:\n:airplane::speech_balloon: :point_right: :stars:\n:japanese_castle::speech_balloon: :thumbsup: :clap::clap::clap::clap:\n:airplane::speech_balloon: :clap::clap::clap::clap:\n",
+    "never give up": ":fist: https://youtu.be/tYzMYcUty6s",
+    "": "",
+    "": "",
+    "": "",
     "test": "testing testing 123"
 }
