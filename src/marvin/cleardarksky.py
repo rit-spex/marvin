3dadceba49ec7539c83b1ec435da730b3e1cc5cf
Caleb Munn <clm1249@g.rit.edu>
2022-02-23 11:47:49 -0500

    Webscraper WIP

--- src/marvin/cleardarksky.py
+++ src/marvin/cleardarksky.py
@@ -1,3 +1,5 @@
+from calendar import month
+from sched import scheduler
 import requests
 
 from bs4 import BeautifulSoup
@@ -6,8 +8,20 @@ response = requests.get("https://www.cleardarksky.com/c/RchstrHYkey.html")
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-for r in soup.find_all('map'):
-    print(type(r))
-    print(r['title'])
+result = soup.find_all('area')
+
+# print(result)
+
+schedule = {}
+
+for r in result:
+    temp_str = str(r.get('title')) + str(r.get('coords'))
+    temp = temp_str.split(' ', 1)
+    schedule[temp[0]] = temp_str
+
+
+
+
+
 
 #result = lxml_html.xpath("/html")#body/table[2]/tbody/tr[1]/td[2]/table/tbody/tr[2]/td/noindex/map")
