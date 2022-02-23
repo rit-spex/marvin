fc9552b63c9749569125f6d7a9a0c85ab819f5fd
Joshua Yoder <me@jpfyoder.com>
2022-02-22 19:56:04 -0500

    add: dependencies

--- src/marvin/cleardarksky.py
+++ src/marvin/cleardarksky.py
@@ -7,6 +7,7 @@ response = requests.get("https://www.cleardarksky.com/c/RchstrHYkey.html")
 soup = BeautifulSoup(response.text, 'html.parser')
 
 for r in soup.find_all('map'):
-    print(r)
+    print(type(r))
+    print(r['title'])
 
 #result = lxml_html.xpath("/html")#body/table[2]/tbody/tr[1]/td[2]/table/tbody/tr[2]/td/noindex/map")
