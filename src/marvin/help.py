87db8e9e2a24898e1168d01b7a2c3198c7b628a6
Stevie Alvarez <stevie.6strings@gmail.com>
2022-04-12 19:52:28 -0400

    fixed indentation

--- src/marvin/help.py
+++ src/marvin/help.py
@@ -9,11 +9,12 @@ def plz_help(arg: str) -> str:
   s = "Usage: "
   if (arg == None) or (arg == "help"):
     s+="$[COMMAND] [OPTIONS...]\n"
-    s+="Use: '$help [COMMAND]' for more information on each command (there probably won't be much more info other than what you'll find here)"
+    s+="Use: '$help [COMMAND]' for more information on each command (there probably won't be much more info other than what you'll find here)\n\n"
     s+="\tapod -- *Gets the NASA Astronomy Photo of the Day*\n\n"
     s+="\tminecraft [OPTIONS...] -- *Easily manage the minecraft server whitelist*\n\t\twhitelist [USERNAME] - Add minecraft account of the given username to the whitelist\n\n"
     s+="\tweather -- *Get the upcoming weather conditions to see if it'll be a good night to go observing.* ***Currently under development***\n\n"
     s+="\tStatic Responses ( __**NOT A SINGLE COMMAND**__, COLLECTION OF COMMANDS )\n\t\tThis list of commands is too long to print out, idk just check the github or something till I get permission to slap ~80-100 lines in here."
+  
   else:
     # should switch to a 'match' conditional, but need python 10 installed (idk what we're using on the server atm....)
     if (arg == "apod"):
