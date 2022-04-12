2c55ee6c1d9eacef53a2d38d0d23ecaa87b31a14
Stevie Alvarez <stevie.6strings@gmail.com>
2022-04-12 19:04:47 -0400

    uncommented weather doc

--- src/marvin/help.py
+++ src/marvin/help.py
@@ -11,7 +11,7 @@ def plz_help() -> str:
   
   s+="\tAPOTD -- *Gets the NASA Astronomy Photo of the Day*\n\n"
   s+="\tminecraft [OPTIONS] -- *Easily manage the minecraft server whitelist*\n\t\twhitelist [USERNAME] - Add minecraft account of the given username to the whitelist\n\n"
-  #s+="\tweather -- *Get the upcoming weather conditions to see if it'll be a good night to go observing*\n\n"
+  s+="\tweather -- *Get the upcoming weather conditions to see if it'll be a good night to go observing*\n\n"
   s+="\tStatic Responses ( __**NOT A SINGLE COMMAND**__, COLLECTION OF COMMANDS )\n\t\tThis list of commands is too long to print out, idk just check the github or something till I get permission to slap ~80-100 lines in here."
 
   # come up with a method of getting the documentation of each .py file in the src dir and using that instead?
