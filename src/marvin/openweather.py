b7aeede050e007694a0b3fe52c907f639066a273
Stevie Alvarez <stevie.6strings@gmail.com>
2022-10-02 14:45:38 -0400

    get OW api key

--- src/marvin/openweather.py
+++ src/marvin/openweather.py
@@ -11,7 +11,7 @@ def weather() -> str:
   """
   lat = 43.1566
   lon = 77.6088
-  #key = os.getenv("OW_TOKEN")
+  key = os.getenv("OW_TOKEN")
   string = "Weather:\n"
 
   response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat={" + lat + "&lon="+ lon + "&appid=" + key)
